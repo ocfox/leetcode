26bcb4317e5988bea6b2f955856e3ef8bd7cfef1
ocfox <ocfox@outlook.com>
2021-12-06 22:42:45 +0800

    clean main

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,3 @@
 fn main() {
-    pub fn truncate_sentence(s: String, k: i32) -> String {
-        let mut count = 0;
-        let mut res = String::new();
-        for char in s.chars() {
-            if char == ' ' {
-                count += 1;
-            }
-            res.push(char);
-            if count == k {
-                break;
-            }
-        }
-        res.trim().to_string()
-    }
+    todo!()
 }
