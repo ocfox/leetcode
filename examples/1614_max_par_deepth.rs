5b16c81cef7474a88903413ffc3034e55486f32c
ocfox <ocfox@outlook.com>
2022-01-07 21:18:57 +0800

    format

--- examples/1614_max_par_deepth.rs
+++ examples/1614_max_par_deepth.rs
@@ -1,17 +1,21 @@
 pub fn max_depth(s: String) -> i32 {
-        let (mut res, mut now) = (0, 0);
-        for char in s.chars() {
-            if char == '(' {
-                now += 1;
-                res = res.max(now);
-            } else if char == ')' {
-                now -= 1;
-                res = res.max(now);
-            }
+    let (mut res, mut now) = (0, 0);
+    for char in s.chars() {
+        if char == '(' {
+            now += 1;
+            res = res.max(now);
+        } else if char == ')' {
+            now -= 1;
+            res = res.max(now);
         }
-        if res > 0 { res } else { 0 }
     }
+    if res > 0 {
+        res
+    } else {
+        0
+    }
+}
 fn main() {
     let test = "(4)+(5)-(((3)))".to_string();
-    println!("{}",max_depth(test));
+    println!("{}", max_depth(test));
 }
