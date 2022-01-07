82665d9fd9c83e1ecf0a95cb7c5728435c78649d
ocfox <ocfox@outlook.com>
2022-01-07 21:08:13 +0800

    1614 max depth

---
+++ examples/1614_max_par_deepth.rs
@@ -0,0 +1,17 @@
+pub fn max_depth(s: String) -> i32 {
+        let (mut res, mut now) = (0, 0);
+        for char in s.chars() {
+            if char == '(' {
+                now += 1;
+                res = res.max(now);
+            } else if char == ')' {
+                now -= 1;
+                res = res.max(now);
+            }
+        }
+        if res > 0 { res } else { 0 }
+    }
+fn main() {
+    let test = "(4)+(5)-(((3)))".to_string();
+    println!("{}",max_depth(test));
+}
