10981906982a508b99a79745f5389f03c62a2ea7
ocfox <i@ocfox.me>
2022-01-25 14:13:40 +0800

    34:Fmt & New test

--- examples/34_array.rs
+++ examples/34_array.rs
@@ -1,14 +1,24 @@
+pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut n = Vec::new();
+    for i in 0..nums.len() {
+        if nums[i] == target {
+            n.push(i);
+        }
+    }
+    if n.len() == 0 {
+        return vec![-1, -1];
+    }
+    vec![n[0] as i32, n[n.len() - 1] as i32]
+}
 fn main() {
-    pub fn search_range(nums: Vec<i32>, target: i32) -> Vec<i32> {
-        let mut n = Vec::new();
-        for i in 0..nums.len() {
-            if nums[i] == target {
-                n.push(i);
-            }
-        }
-        if n.len() == 0 {
-            return vec![-1, -1];
-        }
-        vec![n[0] as i32, n[n.len() - 1] as i32]
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::search_range;
+
+    #[test]
+    fn range_test() {
+        assert_eq!(search_range(vec![2,3,4,5], 4), vec![2,2]);
     }
 }
